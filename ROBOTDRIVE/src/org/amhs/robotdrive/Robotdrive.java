148902b757011d51e743151740a3fc49b0160224
Developer <Developer@10.0.130.88>
2014-02-01 15:28:46 -0800

    COMMIT

--- ROBOTDRIVE/src/org/amhs/robotdrive/Robotdrive.java
+++ ROBOTDRIVE/src/org/amhs/robotdrive/Robotdrive.java
@@ -23,10 +23,13 @@ public class Robotdrive extends SimpleRobot {
     Victor frontRight = new Victor(2);
     Victor backLeft = new Victor(3);
     Victor backRight = new Victor(4);
+    Victor shooter = new Victor(5);
+    Victor loader = new Victor(6);
     DigitalInput button1 = new DigitalInput(1);
     DriverStationLCD station = DriverStationLCD.getInstance();
     double ch1, ch3, ch4;
     boolean old = false;
+    boolean old4 = false;
     String status;
     public void robotInit() {
         System.out.println("Robot Initialized");
@@ -40,35 +43,77 @@ public class Robotdrive extends SimpleRobot {
      */
     public void operatorControl() {
         while(isOperatorControl() && isEnabled()){
+           
             if(button1.get() != old){
                 System.out.println(button1.get());
                 old = button1.get();
-                station.clear();
-                if(button1.get() == true){
-                    station.println(DriverStationLCD.Line.kUser1,1, "True");
-                }
-                else
-                {
-                    station.println(DriverStationLCD.Line.kUser1,1, "False");
-                }
-                old = button1.get();
             }
+            //driving codex
             ch1 = joy1.getX();
-            ch3 = joy2.getX();
-            ch4 = joy2.getY();
+            ch3 = joy2.getY();
+            ch4 = joy2.getX();
             
-            frontLeft.set((ch3 + ch1 + ch4)*(ch3 + ch1 + ch4));
-            backLeft.set((ch3 + ch1 - ch4)*(ch3 + ch1 - ch4));
-            frontRight.set((ch3 - ch1 - ch4)*(ch3 - ch1 - ch4));
-            backRight.set((ch3 - ch1 + ch4)*(ch3 - ch1 + ch4));
+            frontLeft.set(-(ch3 + ch1 + ch4));//port 1
+            frontRight.set((ch3 - ch1 - ch4));//port 2
+            backLeft.set(-(ch3 + ch1 - ch4));//port 3
+            backRight.set((ch3 - ch1 + ch4));//port 4
             
-        }
+          
+            
+            
+            //PRIMITIVE LOADING CODE
+         /**if(joy1.getRawButton(4) && old4 == false)
+            {
+                loader.set(1.0);
+                old4 = true;
+            }
+            else
+            {
+                if(joy1.getRawButton(4) == false)
+                {
+                    loader.set(0.0);
+                }
+            }
+             */
+            //PRIMITIVE SHOOTING CODE
+            //ALL VALUES ARE UNTESTED AND ARE ROUGH GUESTIMATES
+         /**if(joy1.getRawButton(2) && joy1.getTrigger() && button1.get())
+            {
+                shooter.set(1);
+                Timer.delay(3);
+                shooter.set(0);
+                Timer.delay(.2);
+                shooter.set(-.3);
+                Timer.delay(5);
+                shooter.set(0);
+            }
+            */
+    }
     }
     
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
-    
+   
+    }
+    public double specialSquare(double setMe)
+    {
+        if(setMe > .05)
+            {
+                return setMe * setMe;
+            }
+            else
+            {
+                if(setMe < -.05)
+                {
+                    return setMe * setMe * -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
     }
 }
+
